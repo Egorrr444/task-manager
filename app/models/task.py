00115361e1fcb6a5e905b7ff2efa3555aaa74f60
Egor <egordmitriev04@bk.ru>
2026-04-11 02:36:41 +0500

    feat: add SQLAlchemy setup and Task database model

---
+++ app/models/task.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Boolean, Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# строка подключения
+engine = create_engine("sqlite:///./test.db", connect_args={"check_same_thread": False}, echo=True)
+
+
+# создаем движок SqlAlchemy
+Base = declarative_base()
+
+
+# создаем класс сессии
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
+
+
+# создаем модель, объекты которой будут храниться в бд
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, index=True)
+    completed = Column(Boolean, default=False)
+
+Base.metadata.create_all(bind=engine)
