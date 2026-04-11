74a544c2979c99c8d456b2108a68eddb390609e2
Egor <egordmitriev04@bk.ru>
2026-04-11 22:48:04 +0500

    move create_all in main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from app.db.database import Base, engine
 from app.routers import tasks
+from app.models.task import Task  # needed for SQLAlchemy
 
 app = FastAPI()
 
-
-
 app.include_router(tasks.router)
+
+Base.metadata.create_all(bind=engine)
