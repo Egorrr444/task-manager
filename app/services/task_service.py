0bf148fb8c19e485f0bc2cb5997712aa1d63fcc9
Egor <egordmitriev04@bk.ru>
2026-04-12 23:43:05 +0500

    Внедрение сессии БД через Depends и упрощение слоя сервисов

--- app/services/task_service.py
+++ app/services/task_service.py
@@ -1,62 +1,57 @@
 from fastapi import HTTPException
 from app.models.task import Task 
-from app.db.database import SessionLocal
 from app.schemas.task import TaskCreate, TaskUpdate
+from sqlalchemy.orm import Session
 
 
 
-def create_task(task: TaskCreate):
+def create_task(task: TaskCreate, db: Session):
    
-    with SessionLocal() as session:
+    
         new_task = Task(
         title=task.title,
         description=task.description,
         completed=False
     )  
     
-    session.add(new_task)
-    session.commit()
-    session.refresh(new_task)
-    return new_task
+        db.add(new_task)
+        db.commit()
+        db.refresh(new_task)
+        return new_task
 
 
 
-def get_all_tasks():
-    with SessionLocal() as session:
-        return session.query(Task).all()
+def get_all_tasks(db: Session):
+    return db.query(Task).all()
     
 
-def get_task(task_id: int):
-    with SessionLocal() as session:
-        task = session.query(Task).filter(Task.id == task_id).first()
+def get_task(task_id: int, db: Session):
+        task = db.query(Task).filter(Task.id == task_id).first()
         if task is None:
             raise HTTPException(status_code=404, detail="Task not found")
         return task
     
-def delete_task(task_id: int):
-    with SessionLocal() as session:
-        task = session.query(Task).filter(Task.id == task_id).first()
+def delete_task(task_id: int, db: Session):
+        task = db.query(Task).filter(Task.id == task_id).first()
         if task is None:
             raise HTTPException(status_code=404, detail="Task not found")
-        session.delete(task)
-        session.commit()
+        db.delete(task)
+        db.commit()
         return task
 
-def update_task(task_id: int, task_update: TaskUpdate):
-    with SessionLocal() as session:
-        task = session.query(Task).filter(Task.id == task_id).first()
+def update_task(task_id: int, task_update: TaskCreate, db: Session):
+        task = db.query(Task).filter(Task.id == task_id).first()
         if task is None:
             raise HTTPException(status_code=404, detail="Task not found")
         task.title = task_update.title
         task.description = task_update.description
         task.completed = task_update.completed
-        session.commit()
-        session.refresh(task)
+        db.commit()
+        db.refresh(task)
         return task
     
-def update_task(task_id: int, task_update: TaskUpdate):
-    with SessionLocal() as session:
-        task = session.query(Task).filter(Task.id == task_id).first()
+def patch_task(task_id: int, task_update: TaskUpdate, db: Session):
+        task = db.query(Task).filter(Task.id == task_id).first()
         if task is None:
             raise HTTPException(status_code=404, detail="Task not found")
         if task_update.title is not None:
@@ -65,6 +60,6 @@ def update_task(task_id: int, task_update: TaskUpdate):
             task.description = task_update.description
         if task_update.completed is not None:
             task.completed = task_update.completed
-        session.commit()
-        session.refresh(task)
+        db.commit()
+        db.refresh(task)
         return task
